95744e656a217deff3381cd12fa64ec4a58587e8
Levan Didebashvili <levdidebashvili@gmail.com>
2025-04-23 17:27:39 +0400

    feature/max-w-full

--- src/views/Home/organisms/OfferService/OfferService.jsx
+++ src/views/Home/organisms/OfferService/OfferService.jsx
@@ -1,6 +1,6 @@
 const OfferService = () => {
   return (
-    <div className="flex flex-row justify-center items-center bg-[#EFF9FB] mx-auto pb-[25px] lg:max-w-[1920px] lg:min-h-[840px]">
+    <div className="flex flex-row justify-center items-center bg-[#EFF9FB] mx-auto pb-[25px] lg:max-w-full lg:min-h-[840px]">
       <div className="flex lg:flex-row flex-col py-[96px] lg:border-[#B3B3B366] lg:border-t-1 lg:border-b-1 lg:max-w-[1556px]">
         <div className="flex flex-col items-center gap-[30px] lg:p-10 lg:p-t-1 py-10 w-full lg:w-1/3 text-center">
           <img src="./src/assets/box.svg" alt="package" />
