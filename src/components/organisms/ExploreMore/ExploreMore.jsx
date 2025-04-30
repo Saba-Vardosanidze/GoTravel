e681287083c01b10dbb827f2c1254d3df6670ee3
Levan Didebashvili <levdidebashvili@gmail.com>
2025-05-01 00:25:49 +0400

    test

---
+++ src/components/organisms/ExploreMore/ExploreMore.jsx
@@ -0,0 +1,35 @@
+
+
+const ExploreMore = () => {
+  return (
+    <div className="flex flex-col justify-start m-auto max-w-[388px]">
+      <div className="flex flex-col gap-[12px] ml-[15px]">
+      <h2 className="font-bold text-[32px]">Explore more</h2>
+      <p className="font-[300] text-[#7D7D7D]">Let’s go on adventure</p>
+       <div className="flex justify-center items-center bg-[#D9D9D9] mt-[40px] rounded-full w-[54px] h-[36px] font-semibold text-white"><img  className="max-w-[22px] h-[12px]" src="./src/assets/naklebi.svg" alt="naklebi" /></div>
+       </div>
+       
+       <div className="flex justify-center items-center bg-[#18ABC6] ml-[15px] rounded-full w-[54px] h-[36px] font-semibold text-white"><img  className="max-w-[22px] h-[12px]" src="./src/assets/naklebi.svg" alt="naklebi" /></div>
+       
+
+<div>
+<div className="flex flex-col gap-[26px]">
+<img src="./src/assets/png/maldives.png" className="justify-center items-center m-auto object-cover" alt="Kudahuvadhoo" />
+<div className="flex gap-[78px]"><h2 className="flex font-semibold text-[24px]">Kudahuvadhoo</h2>
+<p className="flex">$127/Pax</p></div>
+<p className="flex gap-[7px]"><img src="./src/assets/locatione.svg" alt="location" /> Central Province, Maldives </p> 
+
+</div>
+
+
+
+
+
+
+</div>
+
+    </div>
+  )
+}
+
+export default ExploreMore
