f4f7b1082db00164000e5e68043ee49e28744e0c
LiaCBA <moonlightt1083@gmail.com>
2025-05-09 19:59:27 +0400

    feature/VacationersMobile

--- src/components/organisms/Vacationers/Vacationers.jsx
+++ src/components/organisms/Vacationers/Vacationers.jsx
@@ -1,23 +1,25 @@
 const Vacationers = () => {
   return (
-    <div className="flex flex-col items-center mt-[144px] [font-family:var(--font-family-Poppins)]">
-      <h2 className="font-semibold text-[62px]">What Our Vacationers Says</h2>
-      <div className="flex flex-wrap justify-center gap-[79px] mt-[88px]">
-        <div className="flex items-center bg-white shadow-lg pt-[56px] pr-[48px] pb-[56px] pl-[152px] rounded-[24px] max-w-[738px]">
-          <div className="relative mr-[72px]">
-            <img
-              src="./src/assets/woman.svg"
-              alt="woman"
-              className="-ml-[95px] max-w-[190px] max-h-[187px]"
-            />
-          </div>
-          <div className="flex flex-col gap-[56px]">
+    <div className="flex flex-col items-center mt-[96px] w-full max-w-[393px] [font-family:var(--font-family-Poppins)]">
+      <h2 className="mb-[64px] font-semibold text-[38px] text-center">
+        What Our Vacationers Says
+      </h2>
+      <div className="flex flex-wrap justify-center gap-[64px] mt-[64px] w-full max-w-[361px]">
+        <div className="flex items-center bg-white shadow-lg mb-[64px] pt-[104px] pr-[24px] pb-[64px] pl-[24px] rounded-[24px] max-w-[361px]">
+          <div className="flex flex-col justify-center items-center gap-[32px]">
+            <div className="relative mr-[72px]">
+              <img
+                src="./src/assets/woman.svg"
+                alt="woman"
+                className="bottom-[10px] left-[-130px] absolute max-w-[190px] max-h-[187px]"
+              />
+            </div>
             <p className="text-gray-700">
               Our experience with GoTravel has been nothing short of
               exceptional. We were able to find & book our annual vacation to
               Thailand in under an hour!
             </p>
-            <p className="font-bold text-[17px]">
+            <p className="w-[313px] font-bold text-[17px]">
               Linh Chuong /
               <span className="font-normal text-gray-500">
                 Trip to Thailand
@@ -25,21 +27,21 @@ const Vacationers = () => {
             </p>
           </div>
         </div>
-        <div className="flex items-center bg-white shadow-lg pt-[56px] pr-[48px] pb-[56px] pl-[152px] rounded-[24px] max-w-[738px]">
-          <div className="relative mr-[72px]">
-            <img
-              src="./src/assets/man.svg"
-              alt="man"
-              className="-ml-[95px] max-w-[190px] max-h-[187px]"
-            />
-          </div>
-          <div className="flex flex-col gap-[56px]">
+        <div className="flex items-center bg-white shadow-lg pt-[104px] pr-[24px] pb-[64px] pl-[24px] rounded-[24px] max-w-[361px]">
+          <div className="flex flex-col justify-center items-center gap-[32px]">
+            <div className="relative mr-[72px]">
+              <img
+                src="./src/assets/man.svg"
+                alt="man"
+                className="bottom-[10px] left-[-130px] absolute max-w-[190px] max-h-[187px]"
+              />
+            </div>
             <p className="text-gray-700">
               I booked a 2 week vacation to the Maldives for me and my fiance
               with GoTravel. Very fair price and stunning resort. Felt amazing
               to unwind and get away!
             </p>
-            <p className="font-bold text-[17px]">
+            <p className="w-[313px] font-bold text-[17px]">
               Alex Johnson /
               <span className="font-normal text-gray-500">
                 Maldives Getaway
